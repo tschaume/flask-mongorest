1103d41a00098c5bf1b7a52dacb50a0a3a9e51bd
Patrick Huck <phuck@lbl.gov>
2020-07-15 01:17:58 -0700

    use params and set_value

--- flask_mongorest/resources.py
+++ flask_mongorest/resources.py
@@ -911,7 +911,7 @@ class Resource(object):
         extra = {}
 
         if self.view_method == methods.Download:
-            fmt = self.params.get('format')
+            fmt = params.get('format')
             if fmt not in self.download_formats:
                 raise ValueError(f'`format` must be one of {self.download_formats}')
 
@@ -1056,10 +1056,7 @@ class Resource(object):
                 update = True
 
             if update:
-                if getattr(obj, field, None) is not None:
-                    setattr(obj, field, value)
-                else:
-                    obj[field] = value
+                set_value(obj, field, value)
                 self._dirty_fields.append(field)
 
         if save:
